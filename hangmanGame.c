c4f7eade805b505e6b2c5ca8c5d78239ae308ca2
Cameron Hoang <51996720+Artlist-creator@users.noreply.github.com>
2019-09-12 16:12:44 -0700

    Upload randond function

--- hangmanGame.c
+++ hangmanGame.c
@@ -4,49 +4,71 @@
 
 // Hangman game
 
-struct word {
-	size_t len;
-	char * text;
+struct word
+{
+    size_t len;
+    char *text;
 };
 
-struct wordList {
-	size_t count;
-	struct word * list[90000];
-	size_t maxLen;
-} * words // declare words with wordlist;
+struct wordList
+{
+    struct word *index[90000];
+    size_t lenOfWord;
+    size_t len;
+} * words; // declare words with wordlist;
 
 // -----------------------------------
-char loadWords(const char *fileName){
+
+void srandom(unsigned int seed);
+
+int getRandom(int len)
+{
+    /* Returns a number within a range of (0, len) */
+    srand(0);
+    int num = rand() % (len + 1);
+    return num;
+}
+
+void loadWords(char const *fileName)
+{
+
     /* Returns a list of valid words. Words are strings of uppercase letters.
-    
+    Assumes every word has maxium of 29 characters
     Depending on the size of the word list, this function may
     take a while to finish. */
+
     printf("Loading word list from file...");
     char temp[30];
-    size_t index = 0;
+    size_t counter = 0;
     words = malloc(sizeof(struct wordList));
-    words.maxLen = 1;
+    words->lenOfWord = 1;
 
-    FILE * LIST = fopen(fileName, "r");
-    if (LIST == NULL){
+    FILE *LIST = fopen(fileName, "r");
+    if (LIST == NULL)
+    {
         printf("File not found!");
         exit(1);
     }
-    while (fscanf(LIST, "%s",temp)!= EOF)
+    while (fscanf(LIST, "%s", temp) != EOF)
     {
+        //Optimize memory in words's struct type
         size_t len = strlen(temp);
-        if (words.maxlen < len){
-            words.maxlen =len;
+        if (words->lenOfWord < len)
+        {
+            words->lenOfWord = len;
         }
-        words.list[index] = malloc(sizeof(struct word));
-		words.list[index].text = malloc(len*sizeof(char)+1);
 
+        //Allocate memory for words's elements
+        words->index[counter] = malloc(sizeof(struct word));
+        words->index[counter]->text = malloc(len * sizeof(char) + 1);
 
+        //Get String and len for words
+        strcpy(words->index[counter]->text, temp);
+        words->index[counter]->len = len;
+        counter++;
     }
-    
-    
+    words->len = counter;
 }
-
 
 // char chooseWord(wordlist):
 //     """
@@ -66,8 +88,6 @@ char loadWords(const char *fileName){
 //     returns: boolean, True if all the letters of secretWord are in lettersGuessed;
 //       False otherwise */
 
- 
-            
 // char getGuessedWord(secretWord, lettersGuessed):
 
 //    /*  secretWord: string, the word the user is guessing
@@ -81,23 +101,22 @@ char loadWords(const char *fileName){
 //     returns: string, comprised of letters that represents what letters have not
 //       yet been guessed. */
 
-
 // char hangman(secretWord):
 
 //     /* secretWord: string, the secret word to guess.
 
 //     Starts up an interactive game of Hangman.
 
-//     * At the start of the game, let the user know how many 
+//     * At the start of the game, let the user know how many
 //       letters the secretWord contains.
 
 //     * Ask the user to supply one guess (i.e. letter) per round.
 
-//     * The user should receive feedback immediately after each guess 
+//     * The user should receive feedback immediately after each guess
 //       about whether their guess appears in the computers word.
 
-//     * After each round, you should also display to the user the 
-//       partially guessed word so far, as well as letters that the 
+//     * After each round, you should also display to the user the
+//       partially guessed word so far, as well as letters that the
 //       user has not yet guessed.
 
 //     Follows the other limitations detailed in the problem write-up. */
